a3ec7226aa6e329f8e4b1cba6272a1b3221a3e9d
Calesi <jim.youngman1@gmail.com>
2017-01-21 10:35:01 +1300

    Station Script

---
+++ WetWaves/Assets/Scripts/StationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationController : MonoBehaviour {
+
+    public enum bandwidth
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public AudioClip[] stationClips;
+    public bool randomiseClipOrder = false;
+    public bandwidth stationBandWidth = bandwidth.High;
+    public int frequency = 0;
+
+    private AudioSource source;
+
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
