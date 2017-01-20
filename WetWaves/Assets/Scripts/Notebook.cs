fa5a97ae61f53fea8409d404414351ee30a63ac6
Isabel <izhu678@aucklanduni.ac.nz>
2017-01-21 11:38:03 +1300

    Added basic note taking functionality

---
+++ WetWaves/Assets/Scripts/Notebook.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Notebook : MonoBehaviour {
+
+    public GameObject textfield;
+    public GameObject notepad;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        // Display text input field and piece of note paper
+        textfield.SetActive(true);
+    }
+}
---
+++ WetWaves/Assets/Scripts/TextField.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextField : MonoBehaviour {
+
+    public GameObject textDisplayer;
+    string currentText;
+
+    // Use this for initialization
+    void Start () {
+        var input = gameObject.GetComponent<InputField>();
+        currentText = textDisplayer.GetComponent<Text>().text;
+        Debug.Log("start");
+        input.onEndEdit.AddListener(updateTextdisplay);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void updateTextdisplay(string newText)
+    {
+        currentText = currentText + "\n" +  newText;
+        textDisplayer.GetComponent<Text>().text = currentText;
+
+        Debug.Log(currentText);
+        Debug.Log("end");
+        gameObject.SetActive(false);
+    }
+    
+}
