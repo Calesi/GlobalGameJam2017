978ce1e06809fbaafdb7aa5fdc6f537711658327
Event <event@mds.co.nz>
2017-01-22 12:16:40 +1300

    Implemented fade in screen, camera rotation and centering screen

---
+++ WetWaves/Assets/ResetCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ResetCamera : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject camera;
+    Vector3 originalPosition;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        camera.transform.rotation = new Quaternion(0.2f, 0, 0, 1.0f);
+    }
+}
