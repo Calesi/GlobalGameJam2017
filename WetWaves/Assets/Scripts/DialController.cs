943cac8d159f47020cbaef5e08e6c5f3aa4c7f24
Calesi <jim.youngman1@gmail.com>
2017-01-21 15:42:03 +1300

    Tape Recorder stuff

--- WetWaves/Assets/Scripts/DialController.cs
+++ WetWaves/Assets/Scripts/DialController.cs
@@ -5,243 +5,246 @@ using UnityEngine.UI;
 
 public class DialController : MonoBehaviour
 {
-  //Publics
-  public GameObject dialLeft;
-  public GameObject dialRight;
-  public GameObject frequencyNotch;
-  public Text frequencyText;
+    //Publics
+    public GameObject dialLeft;
+    public GameObject dialRight;
+    public GameObject frequencyNotch;
+    public Text frequencyText;
 
-  //Sounds
-  public StationController[] stations;
-  public AudioSource staticSource;
-  public AudioClip radioStatic;
+    //Sounds
+    public StationController[] stations;
+    public AudioSource staticSource;
+    public AudioClip radioStatic;
 
-  //Privates
-  private float notchXPosition = -4.0f;
-  private float waitForHoldTimer = 0;
-  private float tuneSpeed = 1;
-  public int currentFrequency;
+    //Privates
+    private float notchXPosition = -4.0f;
+    private float waitForHoldTimer = 0;
+    private float tuneSpeed = 1;
+    public int currentFrequency;
 
-  void Start()
-  {
-    currentFrequency = 0;
-  }
-
-  void Update()
-  {
-    GetInput();
-    UpdateAudio(currentFrequency);
-
-  }
-
-  void GetInput()
-  {
-
-    //Check if single tap or held
-    if (Input.GetKeyDown(KeyCode.A)) //Tapped
+    void Start()
     {
-      if (currentFrequency >= 1)
-      {
-        dialLeft.transform.Rotate(new Vector3(0, 1, 0), tuneSpeed, Space.Self);
-        currentFrequency -= (int)tuneSpeed;
-      }
-      else
-      {
         currentFrequency = 0;
-      }
-      UpdateFrequency();
     }
-    else if (Input.GetKey(KeyCode.A)) //Held
+
+    void Update()
     {
-      if (currentFrequency >= 1)
-      {
-        //Wait a short delay to start fast tuning
-        waitForHoldTimer += Time.deltaTime;
-        if (waitForHoldTimer > 0.2f)
+        if (!TapeRecorder.instance.recording)
         {
-          dialLeft.transform.Rotate(new Vector3(0, 1, 0), tuneSpeed, Space.Self);
-          currentFrequency -= (int)tuneSpeed;
+            GetInput();
+            UpdateAudio(currentFrequency);
         }
-      }
-      else
-      {
-        currentFrequency = 0;
-      }
-      UpdateFrequency();
-    }
-    if (Input.GetKeyUp(KeyCode.A)) //Reset fast timer
-    {
-      waitForHoldTimer = 0;
     }
 
-    //Check if single tap or held
-    if (Input.GetKeyDown(KeyCode.D)) //Tapped
+    void GetInput()
     {
-      if (currentFrequency <= 799)
-      {
-        dialLeft.transform.Rotate(new Vector3(0, 1, 0), -tuneSpeed, Space.Self);
-        currentFrequency += (int)tuneSpeed;
-      }
-      else
-      {
-        currentFrequency = 800;
-      }
-      UpdateFrequency();
-    }
-    else if (Input.GetKey(KeyCode.D)) //Held
-    {
-      if (currentFrequency <= 799)
-      {
-        //Wait a short delay to start fast tuning
-        waitForHoldTimer += Time.deltaTime;
-        if (waitForHoldTimer > 0.2f)
+
+        //Check if single tap or held
+        if (Input.GetKeyDown(KeyCode.A)) //Tapped
         {
-          dialLeft.transform.Rotate(new Vector3(0, 1, 0), -tuneSpeed, Space.Self);
-          currentFrequency += (int)tuneSpeed;
+            if (currentFrequency >= 1)
+            {
+                dialLeft.transform.Rotate(new Vector3(0, 1, 0), tuneSpeed, Space.Self);
+                currentFrequency -= (int)tuneSpeed;
+            }
+            else
+            {
+                currentFrequency = 0;
+            }
+            UpdateFrequency();
         }
-      }
-      else
-      {
-        currentFrequency = 800;
-      }
-      UpdateFrequency();
-    }
-    if (Input.GetKeyUp(KeyCode.D)) //Reset fast timer
-    {
-      waitForHoldTimer = 0;
-    }
-  }
-
-  public void UpdateFrequency()
-  {
-
-    //calculate correct notch position
-    notchXPosition = currentFrequency;
-    notchXPosition /= 100;
-    notchXPosition -= 4.0f;
-    notchXPosition -= frequencyNotch.transform.position.x;
-    Vector3 pos = new Vector3(notchXPosition, 0, 0);
-    frequencyNotch.transform.Translate(pos);
-
-    //Calculate frequency into stations e.g. 459 = 45.9
-    float actualFrequency = (float)currentFrequency;
-    actualFrequency /= 10;
-
-    //Keep the length of the string the same at all times
-    if (actualFrequency == 0)
-    {
-      frequencyText.text = "00.0";
-    }
-    else if (actualFrequency < 10 && actualFrequency == Mathf.FloorToInt(actualFrequency))
-    {
-      frequencyText.text = "0" + actualFrequency.ToString() + ".0";
-    }
-    else if (actualFrequency < 10)
-    {
-      frequencyText.text = "0" + actualFrequency.ToString();
-    }
-    else if (actualFrequency == Mathf.FloorToInt(actualFrequency))
-    {
-      frequencyText.text = actualFrequency.ToString() + ".0";
-    }
-    else
-    {
-      frequencyText.text = actualFrequency.ToString();
-    }
-  }
-
-  void UpdateAudio(int frequency)
-  {
-
-    for (int i = 0; i < stations.Length; i++)
-    {
-      print(string.Format("frequency: {0}, sationFrequency: {1}", frequency, stations[i].frequency));
-      var stationBandwidth = ToFrequencyRange(stations[i].stationBandwidth);
-      if (frequency > stations[i].frequency - stationBandwidth && frequency < stations[i].frequency + stationBandwidth) //Check if close to a radio station
-      {
-        if (frequency == stations[i].frequency) //Are we right on the station
+        else if (Input.GetKey(KeyCode.A)) //Held
         {
-          print("full volume");
-          stations[i].GetSource().volume = 1;
+            if (currentFrequency >= 1)
+            {
+                //Wait a short delay to start fast tuning
+                waitForHoldTimer += Time.deltaTime;
+                if (waitForHoldTimer > 0.2f)
+                {
+                    dialLeft.transform.Rotate(new Vector3(0, 1, 0), tuneSpeed, Space.Self);
+                    currentFrequency -= (int)tuneSpeed;
+                }
+            }
+            else
+            {
+                currentFrequency = 0;
+            }
+            UpdateFrequency();
         }
-        else if (frequency > stations[i].frequency - stationBandwidth && frequency < stations[i].frequency)
+        if (Input.GetKeyUp(KeyCode.A)) //Reset fast timer
         {
-          switch (stations[i].frequency - frequency) //How close to the station are we?
-          {
-            case 1:
-            stations[i].GetSource().volume = 0.4f;
-            break;
-            case 2:
-            stations[i].GetSource().volume = 0.3f;
-            break;
-            case 3:
-            stations[i].GetSource().volume = 0.2f;
-            break;
-            case 4:
-            stations[i].GetSource().volume = 0.1f;
-            break;
-            default:
-            stations[i].GetSource().volume = 0.0f;
-            break;
-          }
+            waitForHoldTimer = 0;
+        }
+
+        //Check if single tap or held
+        if (Input.GetKeyDown(KeyCode.D)) //Tapped
+        {
+            if (currentFrequency <= 799)
+            {
+                dialLeft.transform.Rotate(new Vector3(0, 1, 0), -tuneSpeed, Space.Self);
+                currentFrequency += (int)tuneSpeed;
+            }
+            else
+            {
+                currentFrequency = 800;
+            }
+            UpdateFrequency();
+        }
+        else if (Input.GetKey(KeyCode.D)) //Held
+        {
+            if (currentFrequency <= 799)
+            {
+                //Wait a short delay to start fast tuning
+                waitForHoldTimer += Time.deltaTime;
+                if (waitForHoldTimer > 0.2f)
+                {
+                    dialLeft.transform.Rotate(new Vector3(0, 1, 0), -tuneSpeed, Space.Self);
+                    currentFrequency += (int)tuneSpeed;
+                }
+            }
+            else
+            {
+                currentFrequency = 800;
+            }
+            UpdateFrequency();
+        }
+        if (Input.GetKeyUp(KeyCode.D)) //Reset fast timer
+        {
+            waitForHoldTimer = 0;
+        }
+    }
+
+    public void UpdateFrequency()
+    {
+
+        //calculate correct notch position
+        notchXPosition = currentFrequency;
+        notchXPosition /= 100;
+        notchXPosition -= 4.0f;
+        notchXPosition -= frequencyNotch.transform.position.x;
+        Vector3 pos = new Vector3(notchXPosition, 0, 0);
+        frequencyNotch.transform.Translate(pos);
+
+        //Calculate frequency into stations e.g. 459 = 45.9
+        float actualFrequency = (float)currentFrequency;
+        actualFrequency /= 10;
+
+        //Keep the length of the string the same at all times
+        if (actualFrequency == 0)
+        {
+            frequencyText.text = "00.0";
+        }
+        else if (actualFrequency < 10 && actualFrequency == Mathf.FloorToInt(actualFrequency))
+        {
+            frequencyText.text = "0" + actualFrequency.ToString() + ".0";
+        }
+        else if (actualFrequency < 10)
+        {
+            frequencyText.text = "0" + actualFrequency.ToString();
+        }
+        else if (actualFrequency == Mathf.FloorToInt(actualFrequency))
+        {
+            frequencyText.text = actualFrequency.ToString() + ".0";
         }
         else
         {
-          switch (frequency - stations[i].frequency)//How close to the station are we?
-          {
-            case 1:
-            stations[i].GetSource().volume = 0.4f;
-            break;
-            case 2:
-            stations[i].GetSource().volume = 0.3f;
-            break;
-            case 3:
-            stations[i].GetSource().volume = 0.2f;
-            break;
-            case 4:
-            stations[i].GetSource().volume = 0.1f;
-            break;
-            default:
-            stations[i].GetSource().volume = 0.0f;
-            break;
-          }
+            frequencyText.text = actualFrequency.ToString();
         }
-      }
-      else
-      {
-        //Not near a station
-        stations[i].GetSource().volume = 0.0f;
-      }
     }
-    float tempHeighestVolume = 0;
-    for (int i = 0; i < stations.Length; i++)
+
+    void UpdateAudio(int frequency)
     {
 
-      print(string.Format("Current station volume: {0}", stations[i].GetSource().volume));
-      if(stations[i].GetSource().volume > 0)
-      {
-        if (stations[i].GetSource().volume > tempHeighestVolume) //Keep a value of the current highest volume station
+        for (int i = 0; i < stations.Length; i++)
         {
-          tempHeighestVolume = stations[i].GetSource().volume;
+            //print(string.Format("frequency: {0}, sationFrequency: {1}", frequency, stations[i].frequency));
+            var stationBandwidth = ToFrequencyRange(stations[i].stationBandwidth);
+            if (frequency > stations[i].frequency - stationBandwidth && frequency < stations[i].frequency + stationBandwidth) //Check if close to a radio station
+            {
+                if (frequency == stations[i].frequency) //Are we right on the station
+                {
+                    //print("full volume");
+                    stations[i].GetSource().volume = 1;
+                }
+                else if (frequency > stations[i].frequency - stationBandwidth && frequency < stations[i].frequency)
+                {
+                    switch (stations[i].frequency - frequency) //How close to the station are we?
+                    {
+                        case 1:
+                            stations[i].GetSource().volume = 0.4f;
+                            break;
+                        case 2:
+                            stations[i].GetSource().volume = 0.3f;
+                            break;
+                        case 3:
+                            stations[i].GetSource().volume = 0.2f;
+                            break;
+                        case 4:
+                            stations[i].GetSource().volume = 0.1f;
+                            break;
+                        default:
+                            stations[i].GetSource().volume = 0.0f;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (frequency - stations[i].frequency)//How close to the station are we?
+                    {
+                        case 1:
+                            stations[i].GetSource().volume = 0.4f;
+                            break;
+                        case 2:
+                            stations[i].GetSource().volume = 0.3f;
+                            break;
+                        case 3:
+                            stations[i].GetSource().volume = 0.2f;
+                            break;
+                        case 4:
+                            stations[i].GetSource().volume = 0.1f;
+                            break;
+                        default:
+                            stations[i].GetSource().volume = 0.0f;
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                //Not near a station
+                stations[i].GetSource().volume = 0.0f;
+            }
         }
-      }
-    }
-    //Set the static volume
-    staticSource.volume = 1 - tempHeighestVolume;
+        float tempHeighestVolume = 0;
+        for (int i = 0; i < stations.Length; i++)
+        {
 
-  }
+            //print(string.Format("Current station volume: {0}", stations[i].GetSource().volume));
+            if (stations[i].GetSource().volume > 0)
+            {
+                if (stations[i].GetSource().volume > tempHeighestVolume) //Keep a value of the current highest volume station
+                {
+                    tempHeighestVolume = stations[i].GetSource().volume;
+                }
+            }
+        }
+        //Set the static volume
+        staticSource.volume = 1 - tempHeighestVolume;
 
-  private int ToFrequencyRange(StationController.bandwidth bandwidth)
-  {
-    switch (bandwidth) {
-      case StationController.bandwidth.High:
-      return 5;
-      case StationController.bandwidth.Medium:
-      return 3;
-      case StationController.bandwidth.Low:
-      return 1;
-      default:
-      return 3;
     }
-  }
+
+    private int ToFrequencyRange(StationController.bandwidth bandwidth)
+    {
+        switch (bandwidth)
+        {
+            case StationController.bandwidth.High:
+                return 5;
+            case StationController.bandwidth.Medium:
+                return 3;
+            case StationController.bandwidth.Low:
+                return 1;
+            default:
+                return 3;
+        }
+    }
 }
---
+++ WetWaves/Assets/Scripts/TapeRecorder.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapeRecorder : MonoBehaviour
+{
+
+    public static TapeRecorder instance;
+
+    public bool recording = false;
+    private bool playing = false;
+    public GameObject[] buttons;
+    private int currentButton = 3;
+
+    [Header("Recording Components")]
+    public AudioSource[] externalSources;
+    public List<AudioSource> sources = new List<AudioSource>();
+    public List<List<AudioClip>> sourceClips = new List<List<AudioClip>>();
+    public List<float> startingTimestamp = new List<float>();
+    public List<float> endingTimestamp = new List<float>();
+    public List<int> currentClips = new List<int>();
+
+    void Start()
+    {
+        for (int i = 0; i < externalSources.Length; i++)
+        {
+            AudioSource newSource = gameObject.AddComponent<AudioSource>() as AudioSource;
+            sources.Add(newSource);
+            List<AudioClip> newClips = new List<AudioClip>();
+            sourceClips.Add(newClips);
+            currentClips.Add(0);
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        CheckClicked();
+        if (recording)
+        {
+            Record();
+        }
+        if (playing)
+        {
+            Playback();
+        }
+    }
+
+    void Playback()
+    {
+
+        for (int i = 0; i < sources.Count; i++)
+        {
+            if (!sources[i].isPlaying)
+            {
+
+                currentClips[i]++;
+                if (currentClips[i] == sourceClips[i].Count)
+                {
+                    currentClips[i] = 0;
+                }
+                sources[i].clip = sourceClips[i][currentClips[i]];
+                sources[i].Play();
+            }
+        }
+    }
+
+    void Record()
+    {
+        for (int i = 0; i < externalSources.Length; i++)
+        {
+            if (sources[i].clip != externalSources[i].clip)
+            {
+                sources[i].clip = externalSources[i].clip;
+                Debug.Log(externalSources[i].clip.name);
+                sourceClips[i].Add(externalSources[i].clip);
+            }
+            if(i == 0)
+            {
+                //Set the starting time of the new source to match actual source
+                for (int j = 0; j < startingTimestamp.Count; j++)
+                {
+                    sources[j].time = startingTimestamp[j];
+                }                
+            }
+        }
+    }
+
+    void CheckClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50.0f))
+            {
+                GameObject objectHit = hit.collider.gameObject;
+                if (objectHit.tag == "Button")
+                {
+                    for (int i = 0; i < buttons.Length; i++)
+                    {
+                        if (buttons[i] == objectHit)
+                        {
+                            buttons[currentButton].transform.Translate(new Vector3(0, 0, -0.25f));
+                            currentButton = i;
+                            buttons[currentButton].transform.Translate(new Vector3(0, 0, 0.25f));
+                            switch (currentButton)
+                            {
+                                case 0: //Record
+                                    playing = false;
+                                    recording = true;
+                                    //Started Recording, take note of starting times for each source
+                                    startingTimestamp.Clear();
+                                    for (int k = 0; k < sourceClips.Count; k++)
+                                    {
+                                        sourceClips[k].Clear();
+                                    }
+                                    for (int j = 0; j < externalSources.Length; j++)
+                                    {
+                                        startingTimestamp.Add(externalSources[j].time);
+                                        sources[j].volume = externalSources[j].volume;
+                                    }
+                                    break;
+                                case 1: //Play
+                                    for (int j = 0; j < sources.Count; j++)
+                                    {
+                                        sources[j].time = startingTimestamp[j];
+                                        sources[j].clip = sourceClips[j][0];
+                                        sources[j].Play();
+                                    }
+                                    endingTimestamp.Clear();
+                                    for (int l = 0; l < externalSources.Length; l++)
+                                    {
+                                        endingTimestamp.Add(externalSources[l].time);
+                                    }
+                                    playing = true;
+                                    recording = false;
+                                    break;
+                                case 2: //Reset
+                                    endingTimestamp.Clear();
+                                    for (int l = 0; l < externalSources.Length; l++)
+                                    {
+                                        endingTimestamp.Add(externalSources[l].time);
+                                        sources[l].volume = externalSources[l].volume;
+                                        sources[l].Stop();
+                                    }
+                                    startingTimestamp.Clear();
+                                    for (int k = 0; k < sourceClips.Count; k++)
+                                    {
+                                        sourceClips[k].Clear();
+                                    }
+                                    playing = false;
+                                    recording = false;
+                                    break;
+                                case 3: //Stop
+                                    endingTimestamp.Clear();
+                                    for (int l = 0; l < externalSources.Length; l++)
+                                    {
+                                        endingTimestamp.Add(externalSources[l].time);
+                                        sources[l].volume = externalSources[l].volume;
+                                        sources[l].Stop();
+                                    }
+                                    playing = false;
+                                    recording = false;
+                                    break;
+                                default:
+                                    playing = false;
+                                    recording = false;
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
