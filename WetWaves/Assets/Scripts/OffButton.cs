9341a4c5395747409ba4bb00136fe4324c35bc79
Calesi <jim.youngman1@gmail.com>
2017-01-22 23:40:10 +1300

    Final build, all working and pretty

---
+++ WetWaves/Assets/Scripts/OffButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffButton : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckClicked();
+	}
+
+    void CheckClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50.0f))
+            {
+                GameObject objectHit = hit.collider.gameObject;
+                if (objectHit.tag == "OffButton")
+                {
+                    Application.Quit();
+                }
+
+            }
+        }
+    }
+}
